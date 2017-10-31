0a7d316ef5ddca136a396ab21aba103a10d77a1e
Songkyo Lee <songkyo.lee@gmail.com>
2017-10-31 14:17:56 -0400

    quick shell scripts

--- PionSimulation/run_rootAna.sh
+++ PionSimulation/run_rootAna.sh
@@ -2,18 +2,16 @@
 
 set initfile=0
 #set endfile=500
-set endfile=200
-#set endfile=50
+set endfile=200 ##default
+#set endfile=30
 
 set cemccut=0.5
 set ss_ihcalcut=0.3
 set al_ihcalcut=0.15
-### ohcalcut: only used to fill nomip histos!!! (applied to all step)
-### skip if cemcmip && ihcalmip && 80% of true_p is stored in black holes
-#set ohcalcut_25gev=5.0 # only used to fill nomip histos!!! (applied to all step)
-#set ohcalcut_10gev=2.0 # only used to fill nomip histos!!! (applied to all step)
-set ohcalcut_25gev=2.0 # only used to fill nomip histos!!! (applied to all step)
-set ohcalcut_10gev=1.2 # only used to fill nomip histos!!! (applied to all step)
+#set totmipcut_25gev=2.0 # only used to fill nomip histos!!!
+#set totmipcut_10gev=1.2 # only used to fill nomip histos!!!
+set totmipcut_25gev=4.5 # only used to fill nomip histos!!!
+set totmipcut_10gev=1.5 # only used to fill nomip histos!!!
 set nocut=9999.0
 
 ###########################################################
@@ -21,30 +19,30 @@ set nocut=9999.0
 ###########################################################
 
 ### 25 GeV
-root -l -b -q 'rootAna_scale.C("SS310_PiPl_25GeV",'$initfile,''$endfile',false,false,false,'$cemccut','$ss_ihcalcut','$ohcalcut_25gev')'
-root -l -b -q 'rootAna_scale.C("SS310_PiMi_25GeV",'$initfile,''$endfile',false,false,false,'$cemccut','$ss_ihcalcut','$ohcalcut_25gev')'
-root -l -b -q 'rootAna_scale.C("Al_PiPl_25GeV",'$initfile,''$endfile',false,false,false,'$cemccut','$al_ihcalcut','$ohcalcut_25gev')'
-root -l -b -q 'rootAna_scale.C("Al_PiMi_25GeV",'$initfile,''$endfile',false,false,false,'$cemccut','$al_ihcalcut','$ohcalcut_25gev')'
+#root -l -b -q 'rootAna_scale.C("SS310_PiPl_25GeV",'$initfile,''$endfile',false,false,false,'$cemccut','$ss_ihcalcut','$totmipcut_25gev')'
+#root -l -b -q 'rootAna_scale.C("SS310_PiMi_25GeV",'$initfile,''$endfile',false,false,false,'$cemccut','$ss_ihcalcut','$totmipcut_25gev')'
+#root -l -b -q 'rootAna_scale.C("Al_PiPl_25GeV",'$initfile,''$endfile',false,false,false,'$cemccut','$al_ihcalcut','$totmipcut_25gev')'
+#root -l -b -q 'rootAna_scale.C("Al_PiMi_25GeV",'$initfile,''$endfile',false,false,false,'$cemccut','$al_ihcalcut','$totmipcut_25gev')'
 ### 10 GeV
-root -l -b -q 'rootAna_scale.C("SS310_PiPl_10GeV",'$initfile,''$endfile',false,false,false,'$cemccut','$ss_ihcalcut','$ohcalcut_10gev')'
-root -l -b -q 'rootAna_scale.C("SS310_PiMi_10GeV",'$initfile,''$endfile',false,false,false,'$cemccut','$ss_ihcalcut','$ohcalcut_10gev')'
-root -l -b -q 'rootAna_scale.C("Al_PiPl_10GeV",'$initfile,''$endfile',false,false,false,'$cemccut','$al_ihcalcut','$ohcalcut_10gev')'
-root -l -b -q 'rootAna_scale.C("Al_PiMi_10GeV",'$initfile,''$endfile',false,false,false,'$cemccut','$al_ihcalcut','$ohcalcut_10gev')'
+#root -l -b -q 'rootAna_scale.C("SS310_PiPl_10GeV",'$initfile,''$endfile',false,false,false,'$cemccut','$ss_ihcalcut','$totmipcut_10gev')'
+#root -l -b -q 'rootAna_scale.C("SS310_PiMi_10GeV",'$initfile,''$endfile',false,false,false,'$cemccut','$ss_ihcalcut','$totmipcut_10gev')'
+#root -l -b -q 'rootAna_scale.C("Al_PiPl_10GeV",'$initfile,''$endfile',false,false,false,'$cemccut','$al_ihcalcut','$totmipcut_10gev')'
+#root -l -b -q 'rootAna_scale.C("Al_PiMi_10GeV",'$initfile,''$endfile',false,false,false,'$cemccut','$al_ihcalcut','$totmipcut_10gev')'
 
 ###########################################################
 ########### STEP 2) ohcal scale + cemc mip cut
 ###########################################################
 
 ### 25 GeV
-root -l -b -q 'rootAna_scale.C("SS310_PiPl_25GeV",'$initfile,''$endfile',false,false,true,'$cemccut','$nocut','$ohcalcut_25gev')'
-root -l -b -q 'rootAna_scale.C("SS310_PiMi_25GeV",'$initfile,''$endfile',false,false,true,'$cemccut','$nocut','$ohcalcut_25gev')'
-root -l -b -q 'rootAna_scale.C("Al_PiPl_25GeV",'$initfile,''$endfile',false,false,true,'$cemccut','$nocut','$ohcalcut_25gev')'
-root -l -b -q 'rootAna_scale.C("Al_PiMi_25GeV",'$initfile,''$endfile',false,false,true,'$cemccut','$nocut','$ohcalcut_25gev')'
+root -l -b -q 'rootAna_scale.C("SS310_PiPl_25GeV",'$initfile,''$endfile',false,false,true,'$cemccut','$ss_ihcalcut','$totmipcut_25gev')'
+root -l -b -q 'rootAna_scale.C("SS310_PiMi_25GeV",'$initfile,''$endfile',false,false,true,'$cemccut','$ss_ihcalcut','$totmipcut_25gev')'
+root -l -b -q 'rootAna_scale.C("Al_PiPl_25GeV",'$initfile,''$endfile',false,false,true,'$cemccut','$al_ihcalcut','$totmipcut_25gev')'
+root -l -b -q 'rootAna_scale.C("Al_PiMi_25GeV",'$initfile,''$endfile',false,false,true,'$cemccut','$al_ihcalcut','$totmipcut_25gev')'
 ### 10 GeV
-root -l -b -q 'rootAna_scale.C("SS310_PiPl_10GeV",'$initfile,''$endfile',false,false,true,'$cemccut','$nocut','$ohcalcut_10gev')'
-root -l -b -q 'rootAna_scale.C("SS310_PiMi_10GeV",'$initfile,''$endfile',false,false,true,'$cemccut','$nocut','$ohcalcut_10gev')'
-root -l -b -q 'rootAna_scale.C("Al_PiPl_10GeV",'$initfile,''$endfile',false,false,true,'$cemccut','$nocut','$ohcalcut_10gev')'
-root -l -b -q 'rootAna_scale.C("Al_PiMi_10GeV",'$initfile,''$endfile',false,false,true,'$cemccut','$nocut','$ohcalcut_10gev')'
+root -l -b -q 'rootAna_scale.C("SS310_PiPl_10GeV",'$initfile,''$endfile',false,false,true,'$cemccut','$ss_ihcalcut','$totmipcut_10gev')'
+root -l -b -q 'rootAna_scale.C("SS310_PiMi_10GeV",'$initfile,''$endfile',false,false,true,'$cemccut','$ss_ihcalcut','$totmipcut_10gev')'
+root -l -b -q 'rootAna_scale.C("Al_PiPl_10GeV",'$initfile,''$endfile',false,false,true,'$cemccut','$al_ihcalcut','$totmipcut_10gev')'
+root -l -b -q 'rootAna_scale.C("Al_PiMi_10GeV",'$initfile,''$endfile',false,false,true,'$cemccut','$al_ihcalcut','$totmipcut_10gev')'
 
 ###########################################################
 ########### STEP 3) ihcal scale + ohcal scale + NO mip cut
