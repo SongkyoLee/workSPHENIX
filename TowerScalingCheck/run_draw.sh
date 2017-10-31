1a408a293e3603403eddbdf57d5c4ff87f83fcc3
Songkyo Lee <songkyo.lee@gmail.com>
2017-10-31 14:33:11 -0400

    run SS310 no cut

--- TowerScalingCheck/run_draw.sh
+++ TowerScalingCheck/run_draw.sh
@@ -1,8 +1,9 @@
 #!/bin/csh
 
-foreach prefix (MyTree_Al_NotScaled)
+foreach prefix (MyTree_SS310_NotScaled)
+#foreach prefix (MyTree_Al_NotScaled) # pid is float in this file..
 #foreach prefix (100MeV_Al_NotScaled_v3)
-#foreach prefix (OnlyHcalScaled)
+#foreach prefix (OnlyHcalScaled) #100MeV applied
 #foreach prefix (100MeV_Al_NotScaled_v3 OnlyHcalScaled)
   echo "***** Run" $prefix
   root -l -b -q draw_emfrac.C\(\"$prefix\"\)
