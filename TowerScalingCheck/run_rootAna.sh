a8ae2fb4699c962bb9f76b0163eefa382a1055cc
Songkyo Lee <songkyo.lee@gmail.com>
2017-10-31 14:35:26 -0400

    run SS310 no cut

--- TowerScalingCheck/run_rootAna.sh
+++ TowerScalingCheck/run_rootAna.sh
@@ -3,14 +3,16 @@
 set initfile = 0
 set endfile = 1000
 #set endfile = 600
+
+## MyTree_SS310_NotScaled: 600
 ## MyTree_Al_NotScaled: 636
-##MyTree_Al_Scaled_v3: 585
 ## 100MeV_Al_NotScaled_v3: 420
 ## OnlyHcalScaled: 583
 
-foreach prefix (MyTree_Al_NotScaled) ###pid is float in this tree..
+foreach prefix (MyTree_SS310_NotScaled)
+#foreach prefix (MyTree_Al_NotScaled) # pid is float in this file..
 #foreach prefix (100MeV_Al_NotScaled_v3)
-#foreach prefix (OnlyHcalScaled)
+#foreach prefix (OnlyHcalScaled) #100MeV applied
 #foreach prefix (100MeV_Al_NotScaled_v3 OnlyHcalScaled)
   echo "***** Run" $prefix
   root -l -b -q rootAna_emfrac.C\(\"$prefix\",$initfile,$endfile\)
